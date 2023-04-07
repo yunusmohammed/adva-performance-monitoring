8f7381914d494f45e1aa5aa6e0e43160b7caac64
Yunus Maltiti Mohammed <ymm26@cornell.edu>
2023-04-07 17:11:12 -0400

    Create the PM data model

---
+++ Models/PMData.py
@@ -0,0 +1,15 @@
+"""
+Performance Monitoring Data. Contains both instantaneous data and summary statistics of the data within the last 15 minutes of the instant
+"""
+import InstantaneousPMData
+import FifteenMinuteBinnedPMData
+
+
+class PMData:
+    def __init__(self, instantaneous: InstantaneousPMData, fifteen_minute_bin: FifteenMinuteBinnedPMData) -> None:
+        """
+        instantaneous: the instantaneous performance monitoring data
+        fifteen_minute_bin: summary statistics of the last fifteen-minute bin
+        """
+        self.instantaneous = instantaneous
+        self.fifteen_minute_bin = fifteen_minute_bin
